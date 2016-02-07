dfcf7c21c2b0edf78af0cb315dc07b9114c903e7
kkayla <kkayla.williams@icloud.com>
2016-02-06 21:41:20 -0500

    CREATED ATTEMPTS VIEWS WITH FORM TO DISPLAY USERS RESULTS ON ATTEMPTS.SHOW PAGE

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,10 @@ Rails.application.routes.draw do
     resources :voters
     resources :sessions
     resources :candidates
+    resources :quizzes do
+      resources :questions
+      resources :attempts
+    end
 
 
   # Example resource route with options:
