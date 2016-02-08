1b4ff2878968810ad8e913ab02f213cd6c60e572
Dylan Klett <dylanzklett@gmail.com>
2016-02-08 17:20:31 -0500

    cleaned up Quiz model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,6 +1,3 @@
 class Quiz < ActiveRecord::Base
-	has_many :results
-	has_many :quiz_questions
-	has_many :voters, :through => :results, :foreign_key => "voter_id"
-	has_many :attempts
+	belongs_to :voters
 end
