6f6c1d1ebec75139235870b7b95ab04e2e8fb42f
Dylan Klett <dylanzklett@gmail.com>
2016-02-09 14:33:52 -0500

    got libVal conVal edits working

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -14,20 +14,34 @@ class QuizzesController < ApplicationController
     
     @quiz = Quiz.new(quiz_params)
     @quiz.voter_id = current_voter.id
+    @voter = Voter.where(id:current_voter.id).last
     if @quiz.save
-
+      # @answers = [:answer1, :answer2, :answer3, :answer4, :answer5, :answer6, :answer7, :answer8, :answer9, :answer10]
+      params[:quiz].each do |q|
+        if q == "yes"
+          @voter.libVal += 10
+          @voter.save
+        elsif q == "no"
+          @voter.conVal += 10
+          @voter.save
+        else
+          puts "TRUUUUUUUUUUMMMMPPPP"
+        end
+      end
+      # current_voter.libVal=indexcount(@answers, L)*10;
+      # current_voter.conVal=indexcount(@answers, V)*10
 
       # libVals for each candidate: Trump: 1, Cruz: 15, Rubio: 30, Clinton: 80, Sanders: 99. conVals fill out the rest of the metric to 100.
-      if current_voter.libVal > 80
-        redirect_to candidate_path(lname:"Sanders")
-      elsif current_voter.conVal > 85
-        redirect_to candidate_path(lname:"Trump")
-      elsif current_voter.libVal > 30 && current_voter.conVal > 1
-        redirect_to candidate_path(lname:"Clinton")
-      elsif current_voter.libVal > 15 && current_voter.conVal > 20
-        redirect_to candidate_path(lname:"Rubio")
-      elsif current_voter.conVal > 70 && current_voter.conVal < 99
-        redirect_to candidate_path(lname:"Cruz")
+      # if current_voter.libVal > 80
+      #   redirect_to candidate_path(lname:"Sanders")
+      # elsif current_voter.conVal > 85
+      #   redirect_to candidate_path(lname:"Trump")
+      # elsif current_voter.libVal > 30 && current_voter.conVal > 1
+      #   redirect_to candidate_path(lname:"Clinton")
+      # elsif current_voter.libVal > 15 && current_voter.conVal > 20
+      #   redirect_to candidate_path(lname:"Rubio")
+      # elsif current_voter.conVal > 70 && current_voter.conVal < 99
+      #   redirect_to candidate_path(lname:"Cruz")
 
 
     else
