6376931630de81f0daa93b710f0f76d777ee1a71
kkayla <kkayla.williams@icloud.com>
2016-02-09 11:44:15 -0500

    ADDED FIND_CANDIDATE ACTION INTO QUIZ CONTROLLER WITH NEW LOGI C

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -11,44 +11,46 @@ class QuizzesController < ApplicationController
   end
 
   def create
-    
     @quiz = Quiz.new(quiz_params)
     @quiz.voter_id = current_voter.id
+    @voter= current_voter
+    @voter.libVal =
+    @voter.conVal = 
     if @quiz.save
-
-
       # libVals for each candidate: Trump: 1, Cruz: 15, Rubio: 30, Clinton: 80, Sanders: 99. conVals fill out the rest of the metric to 100.
-      if current_voter.libVal > 80
-        redirect_to candidate_path(lname:"Sanders")
-      elsif current_voter.conVal > 85
-        redirect_to candidate_path(lname:"Trump")
-      elsif current_voter.libVal > 30 && current_voter.conVal > 1
-        redirect_to candidate_path(lname:"Clinton")
-      elsif current_voter.libVal > 15 && current_voter.conVal > 20
-        redirect_to candidate_path(lname:"Rubio")
-      elsif current_voter.conVal > 70 && current_voter.conVal < 99
-        redirect_to candidate_path(lname:"Cruz")
-
-
-    else
-     render :new 
+    #
      flash[:notice] = "There was an error handling your quiz. Please correct the missing fields."
     end
   end
+
+  def find_can
+    if current_voter.libVal > 80
+      redirect_to candidate_path(lname:"Sanders")
+    elsif current_voter.conVal > 85
+      redirect_to candidate_path(lname:"Trump")
+    elsif current_voter.libVal > 30 && current_voter.conVal > 1
+      redirect_to candidate_path(lname:"Clinton")
+    elsif current_voter.libVal > 15 && current_voter.conVal > 20
+      redirect_to candidate_path(lname:"Rubio")
+    elsif current_voter.conVal > 70 && current_voter.conVal < 99
+      redirect_to candidate_path(lname:"Cruz")
+
+  end
+
 
   def show
    @quiz = Quiz.find(current_voter.id)
 
   end
 
- 
+
 
   private
 
   def quiz_params
     params.require(:quiz).permit(:voter_id, :answer1, :answer2, :answer3, :answer4, :answer5, :answer6, :answer7, :answer8, :answer9, :answer10)
   end
-  
+
 end
 
 
